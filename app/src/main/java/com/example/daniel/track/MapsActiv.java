b0bcc404ec78e148eb8baefde37f528f9aedccd7
Dan Laptop <d.oconnor122@googlemail.com>
2018-03-22 20:57:09 +0000

    Updated Wireless Manager

--- app/src/main/java/com/example/daniel/track/MapsActiv.java
+++ app/src/main/java/com/example/daniel/track/MapsActiv.java
@@ -8,6 +8,7 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ public class MapsActiv extends FragmentActivity implements OnMapReadyCallback{
     GoogleMap mMap;
     Switch togTrk;
     ConnectivityManager conMgr;
+    Wifi w = new Wifi();
 
 
     @Override
@@ -39,8 +41,17 @@ public class MapsActiv extends FragmentActivity implements OnMapReadyCallback{
         mapFragment.getMapAsync(this);
         togTrk = (Switch) findViewById(R.id.swTrk);
         conMgr =  (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        togTrk.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
 
-        toggleTracking();
+                if(isChecked){toggleTracking();}
+
+
+            }
+        });
+
+
 
 
     }
@@ -59,6 +70,17 @@ public class MapsActiv extends FragmentActivity implements OnMapReadyCallback{
         Toast.makeText(this, "Tracking toggled!", Toast.LENGTH_SHORT).show();};
         NetworkInfo netInf = conMgr.getActiveNetworkInfo();
         Log.d("NetConn", "con mgr started" + netInf);
+        Log.d("Current Net", netInf.getState().toString());
+        String conStat = netInf.getState().toString();
+        System.out.println(conStat);
+
+        if(conStat == "CONNECTED"){Log.i("NetStatus", "Device has network access!");
+        Toast.makeText(this, "Connected!", Toast.LENGTH_LONG);
+        Context WCont = this;
+        w.GetWirelessData1(WCont);
+
+        }
+        else{Toast.makeText(this , "Check Internet Connectivity!", Toast.LENGTH_LONG);}
 
 
 
--- app/src/main/java/com/example/daniel/track/Wifi.java
+++ app/src/main/java/com/example/daniel/track/Wifi.java
@@ -1,17 +1,77 @@
 package com.example.daniel.track;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.Dictionary;
+import java.util.HashMap;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 /**
  * Created by Daniel on 13/02/2018.
  */
 
 public class Wifi extends User {
 
-    public static void GetWirelessData(){
 
 
+    public void GetWirelessData1(Context cont){
+        WifiManager wifiMan = (WifiManager) cont.getSystemService(cont.WIFI_SERVICE);
+        wifiMan.setWifiEnabled(true);
+        Date time = Calendar.getInstance().getTime();
+        wifiMan.startScan();
 
+
+        Comparator<ScanResult> comparator = new Comparator<ScanResult>() {
+            @Override
+            public int compare(ScanResult lhs, ScanResult rhs) {
+                return (lhs.level > rhs.level ? -1 : (lhs.level == rhs.level ? 0 : 1));
+            }
+        };
+
+
+        List <ScanResult> wifiLis = wifiMan.getScanResults();
+        Collections.sort(wifiLis, comparator);
+        HashMap APhm = new HashMap();
+
+        APhm.put("AP1", wifiLis.get(0).SSID);
+        APhm.put("AP1TIME" , time);
+        APhm.put("AP1BSSID", wifiLis.get(0).BSSID);
+        APhm.put("AP1LEVEL", wifiLis.get(0).level );
+        APhm.put("AP2", wifiLis.get(1).SSID);
+        APhm.put("AP2TIME" , time);
+        APhm.put("AP2BSSID", wifiLis.get(1).BSSID);
+        APhm.put("AP2LEVEL", wifiLis.get(1).level);
+        APhm.put("AP3", wifiLis.get(2).SSID);
+        APhm.put("AP3TIME" , time);
+        APhm.put("AP3BSSID", wifiLis.get(2).BSSID);
+        APhm.put("AP3LEVEL", wifiLis.get(2).level);
+
+
+        System.out.println("This is the first network :::: " + wifiLis.get(0).SSID.toString());
+        System.out.println(APhm.get("AP1BSSID") + " " + APhm.get("AP2BSSID")  + " " + APhm.get("AP3BSSID"));
 
     }
+
+
+//    public HashMap GetApLocations2(HashMap APs){
+//
+//
+//
+//
+//    }
+
 
 
 
