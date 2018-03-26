c88405ea5438f9d50aadad8d26f3c610bea2ed6b
Dan Laptop <d.oconnor122@googlemail.com>
2018-03-26 19:52:07 +0100

    Update get request for wigle. Issue with auth for net detail.

--- app/src/main/java/com/example/daniel/track/Wifi.java
+++ app/src/main/java/com/example/daniel/track/Wifi.java
@@ -7,6 +7,14 @@ import android.content.IntentFilter;
 import android.net.SSLCertificateSocketFactory;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
+import android.preference.PreferenceActivity;
+import android.util.Log;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONObject;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
@@ -26,12 +34,17 @@ import java.util.concurrent.TimeUnit;
 import javax.net.ssl.HostnameVerifier;
 import javax.net.ssl.HttpsURLConnection;
 
+import cz.msebera.android.httpclient.Header;
+import cz.msebera.android.httpclient.auth.AuthScope;
+
 /**
  * Created by Daniel on 13/02/2018.
  */
 
 public class Wifi extends User {
 
+
+    public static String APurl = "https://api.wigle.net/api/v2/network/detail";
 
 
     public void GetWirelessData1(Context cont){
@@ -59,16 +72,18 @@ public class Wifi extends User {
         APhm.put("AP1LEVEL", wifiLis.get(0).level );
         APhm.put("AP2", wifiLis.get(1).SSID);
         APhm.put("AP2TIME" , time);
-        APhm.put("AP2BSSID", wifiLis.get(1).BSSID.toString().replace(":", "%"));
+        APhm.put("AP2BSSID", wifiLis.get(1).BSSID.toString());
         APhm.put("AP2LEVEL", wifiLis.get(1).level);
         APhm.put("AP3", wifiLis.get(2).SSID);
         APhm.put("AP3TIME" , time);
-        APhm.put("AP3BSSID", wifiLis.get(2).BSSID.toString().replace(":", "%"));
+        APhm.put("AP3BSSID", wifiLis.get(2).BSSID.toString());
         APhm.put("AP3LEVEL", wifiLis.get(2).level);
 
 
         System.out.println("This is the first network :::: " + wifiLis.get(0).SSID.toString());
         System.out.println(APhm.get("AP1BSSID") + " " + APhm.get("AP2BSSID")  + " " + APhm.get("AP3BSSID"));
+
+        GetApLocations2(APhm);
 
     }
 
@@ -90,54 +105,30 @@ public class Wifi extends User {
 
     public void GetLocGetReq(HashMap APs, HashMap Aploc){
 
-        HostnameVerifier hostnameVerifier = org.apache.http.conn.ssl.SSLSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER;
-        StringBuilder result = new StringBuilder();
-        URL url = null;
-
-        try {
-            url = new URL("https://api.wigle.net/api/v2/network/detail?netid=" + APs.get("AP1BSSID"));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        }
-        HttpsURLConnection conn = null;
-        try {
-            conn = (HttpsURLConnection) url.openConnection();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        conn.setDoOutput(true);
-            conn.setDoInput(true);
-            conn.setRequestProperty("Content-Type", "application/json");
-        try {
-            conn.setRequestMethod("GET");
-        } catch (ProtocolException e) {
-            e.printStackTrace();
-        }
-        conn.setSSLSocketFactory(SSLCertificateSocketFactory.getInsecure(0, null));
-            conn.setHostnameVerifier(hostnameVerifier);
+        RequestParams params = new RequestParams();
 
 
-        try {
-            conn.connect();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        //End of HTTPS Get Request
+        params.put("netid", APs.get("AP1BSSID"));
 
 
-        //Getting Results Here!
-        try {
-            if (conn.getResponseCode() == 200){
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.setBasicAuth("AID5de8eada7ff8fd72337913007b346e1f", "b9da51d6f0621b4dd95fa517da6a932d", new AuthScope(AuthScope.ANY_REALM, AuthScope.ANY_PORT));
 
+        String CurrURL = "https://api.wigle.net/api/v2/profile/user";
 
+        client.get(CurrURL, params, new JsonHttpResponseHandler() {
 
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response){
+            Log.d("Track", "Great Success" + response.toString());
 
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
 
+            @Override
+            public void onFailure(int statusCode, Header [] headers, Throwable e, JSONObject response){
+                Log.d("Track", "Fail" + e.toString());
+            }
+        });
 
     }
 
@@ -149,7 +140,18 @@ public class Wifi extends User {
 
 
 
+
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
