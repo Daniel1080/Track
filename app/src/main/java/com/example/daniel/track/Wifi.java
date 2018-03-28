21ced8014536eb9d7f87fb3675406ce67fd46d02
Daniel Desktop <d.oconnor122@gmail.com>
2018-03-28 02:03:16 +0100

    Updated to a point. Json query to wigle works but intial get request address didnt. Wigle fault not Daniel fault.

--- app/src/main/java/com/example/daniel/track/Wifi.java
+++ app/src/main/java/com/example/daniel/track/Wifi.java
@@ -47,7 +47,7 @@ public class Wifi extends User {
     public static String APurl = "https://api.wigle.net/api/v2/network/detail";
 
 
-    public void GetWirelessData1(Context cont){
+    public boolean GetWirelessData1(Context cont){
         WifiManager wifiMan = (WifiManager) cont.getSystemService(cont.WIFI_SERVICE);
         wifiMan.setWifiEnabled(true);
         Date time = Calendar.getInstance().getTime();
@@ -62,48 +62,56 @@ public class Wifi extends User {
         };
 
 
+
+
         List <ScanResult> wifiLis = wifiMan.getScanResults();
         Collections.sort(wifiLis, comparator);
+
+        //if(wifiLis.isEmpty() == true){return false;}
+
         HashMap APhm = new HashMap();
 
         APhm.put("AP1", wifiLis.get(0).SSID);
         APhm.put("AP1TIME" , time);
         APhm.put("AP1BSSID", wifiLis.get(0).BSSID.toString().replace(":", "%"));
         APhm.put("AP1LEVEL", wifiLis.get(0).level );
+        APhm.put("AP1LON" , "");
+        APhm.put("AP1LAT", "");
         APhm.put("AP2", wifiLis.get(1).SSID);
         APhm.put("AP2TIME" , time);
-        APhm.put("AP2BSSID", wifiLis.get(1).BSSID.toString());
+        APhm.put("AP2BSSID", wifiLis.get(1).BSSID.toString().replace(":", "%"));
         APhm.put("AP2LEVEL", wifiLis.get(1).level);
+        APhm.put("AP2LON" , "");
+        APhm.put("AP2LAT", "");
         APhm.put("AP3", wifiLis.get(2).SSID);
         APhm.put("AP3TIME" , time);
         APhm.put("AP3BSSID", wifiLis.get(2).BSSID.toString());
         APhm.put("AP3LEVEL", wifiLis.get(2).level);
+        APhm.put("AP3LON" , "");
+        APhm.put("AP3LAT", "");
 
 
         System.out.println("This is the first network :::: " + wifiLis.get(0).SSID.toString());
         System.out.println(APhm.get("AP1BSSID") + " " + APhm.get("AP2BSSID")  + " " + APhm.get("AP3BSSID"));
 
+        //while (APhm.get("m1LON""))
+
         GetApLocations2(APhm);
+
+        return true;
 
     }
 
 
     public HashMap GetApLocations2(HashMap APs){
-        HashMap APloc = new HashMap();
-        APloc.put("AP1lon", 0);
-        APloc.put("AP1lat", 0);
-        APloc.put("AP2lon", 0);
-        APloc.put("AP2lat", 0);
-        APloc.put("AP3lon", 0);
-        APloc.put("AP3lat", 0);
 
-        GetLocGetReq(APs, APloc);
+        GetLocGetReq(APs);
 
-        return APloc;
+        return APs;
 
     }
 
-    public void GetLocGetReq(HashMap APs, HashMap Aploc){
+    public void GetLocGetReq(HashMap APs){
 
         RequestParams params = new RequestParams();
 
@@ -114,7 +122,7 @@ public class Wifi extends User {
         AsyncHttpClient client = new AsyncHttpClient();
         client.setBasicAuth("AID5de8eada7ff8fd72337913007b346e1f", "b9da51d6f0621b4dd95fa517da6a932d", new AuthScope(AuthScope.ANY_REALM, AuthScope.ANY_PORT));
 
-        String CurrURL = "https://api.wigle.net/api/v2/profile/user";
+        String CurrURL = "https://api.wigle.net/api/v2/network/search?onlymine=false&freenet=false&paynet=false&netid="; // Add ap BSSID
 
         client.get(CurrURL, params, new JsonHttpResponseHandler() {
 
